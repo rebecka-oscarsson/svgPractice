30a7b572a04b10cd30ef4f9ce69ce197c4e142d4
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2021-03-25 15:59:15 +0100

    image import using object tag

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+let fur;
+let lefteye;
+const monkeySvg = document.getElementById('monkeySvg');
+
+monkeySvg.addEventListener("load", function() {
+    var monkeyCode = document.getElementById('monkeySvg').contentDocument;
+    fur = monkeyCode.getElementById("fur");
+    lefteye = monkeyCode.querySelector("#leftEye")
+  });
+
+
+
 let buttons = document.querySelectorAll("button");
 
 
 function hide (clickedBtn) {
     switch(clickedBtn){
-    case "furBtn": document.getElementById("fur").classList.toggle("hidden");
+    case "furBtn": fur.classList.toggle("hidden");
     break;
-    case "leftBtn": document.querySelector("#leftEye").classList.toggle("hidden");
+    case "leftBtn": lefteye.classList.toggle("hidden");
     break;
     case "faceBtn": document.getElementById("face").classList.toggle("hidden");
     break;
